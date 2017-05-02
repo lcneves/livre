fd27b8a677e5a3f8a04cbc0eed771a31b2dc3d11
Lucas Neves <lcneves@gmail.com>
2017-05-02 00:04:30 +0000

    Cleanup

--- foxx/world/scripts/teardown.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const graph_module = require('@arangodb/general-graph');
-
-graph_module._drop('world', true); // Also drops collections
-
--- node/scripts/livre3d/themes/liberty/livre3d.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-  render: require('./render3d.js'),
-  getProps: require('./get-props.js'),
-  objects: require('./objects.js'),
-  animate: require('./animate.js')
-};
